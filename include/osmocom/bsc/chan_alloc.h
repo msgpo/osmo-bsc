osmocom/openbsc

HO: Count number of free timeslot on a given BTS
This is needed for handover algorithm to balance free slots and to prevent
congestion of one cell, while other cells still have free capacities.

openbsc/include/openbsc/chan_alloc.h
@@ -35,6 +35,9 @@ struct gsm_bts_trx_ts *ts_alloc(struct gsm_bts *bts,
 /* Regular physical channel (TS) */
 void ts_free(struct gsm_bts_trx_ts *ts);
 
+/* Count number of free TS of given pchan type */
+int lc_count_bts(struct gsm_bts *bts, enum gsm_phys_chan_config pchan);
+
 /* Find an allocated channel for a specified subscriber */
 struct gsm_subscriber_connection *connection_for_subscr(struct gsm_subscriber *subscr);
 

msgpo/osmo-bsc

Change-Id: Ic8bee8a515ee8aa9a99af71756fe60b8dd8f868b

include/osmocom/bsc/chan_alloc.h
@@ -24,6 +24,9 @@
 
 struct gsm_subscriber_connection;
 
+/* Count number of free TS of given pchan type */
+int bts_count_free_ts(struct gsm_bts *bts, enum gsm_phys_chan_config pchan);
+
 /* Allocate a logical channel (SDCCH, TCH, ...) */
 struct gsm_lchan *lchan_alloc(struct gsm_bts *bts, enum gsm_chan_t type, int allow_bigger);
 